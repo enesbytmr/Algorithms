d6bddd10567bdb61fcc2ecbfcbc7b4e46a9b6e77
enesbytmr <enesbytmr@gmail.com>
2021-01-08 13:01:43 +0300

    es5 to es6

--- Algorithm 4.js
+++ Algorithm 4.js
@@ -1,7 +1,7 @@
 //Basic Algorithm Scripting: Repeat a String Repeat a String
 //Repeat a given string str (first argument) for num times (second argument). Return an empty string if num is not a positive number. For the purpose of this challenge, do not use the built-in .repeat() method.
 
-function repeatStringNumTimes(str, num) {
+let repeatStringNumTimes= (str, num) => {
   let newStr='' ;
   if(num > 0){
   for(let i = 0; i< num; i ++){
